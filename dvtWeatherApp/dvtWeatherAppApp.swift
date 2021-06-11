9711b4a541f8b9f13ce0a0bc1ac3147c674cc656
Daniel Jacobs <djacobs@tigerfishsoftware.co.za>
2021-06-11 20:51:14 +0200

    Initial Commit

---
+++ dvtWeatherApp/dvtWeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  dvtWeatherAppApp.swift
+//  dvtWeatherApp
+//
+//  Created by Daniel Jacobs on 2021/06/11.
+//
+
+import SwiftUI
+
+@main
+struct dvtWeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
